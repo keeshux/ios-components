e07e5c14c23cdda5792b06ae16cea2259c0f8363
Davide De Rosa <keeshux@gmail.com>
2014-09-12 13:44:59 +0200

    Added date macros

---
+++ Components/Classes/KSMacros/KSMacrosDates.h
@@ -0,0 +1,115 @@
+//
+// Copyright (c) 2014, Davide De Rosa
+// All rights reserved.
+//
+// This code is distributed under the terms and conditions of the BSD license.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - Year
+
+NSInteger KSDatesYear(NSDate *date);
+
+static inline NSInteger KSDatesCurrentYear() {
+    return KSDatesYear([NSDate date]);
+}
+
+NSMutableArray *KSDatesYears(const NSInteger from, const NSInteger to, const NSInteger step);
+
+static inline NSMutableArray *KSDatesYearsFromYear(const NSUInteger count, const NSUInteger from, const NSInteger step) {
+    return KSDatesYears(from, from + count, step);
+}
+
+static inline NSMutableArray *KSDatesYearsToYear(const NSUInteger count, const NSUInteger to, const NSInteger step) {
+    return KSDatesYears(to - count, to, step);
+}
+
+static inline NSMutableArray *KSDatesYearsFromCurrentYear(const NSUInteger count, const NSInteger step) {
+    return KSDatesYearsFromYear(count, KSDatesCurrentYear(), step);
+}
+
+static inline NSMutableArray *KSDatesYearsToCurrentYear(const NSUInteger count, const NSInteger step) {
+    return KSDatesYearsToYear(count, KSDatesCurrentYear(), step);
+}
+
+static inline NSMutableArray *KSDatesYearsFromYearToCurrentYear(const NSInteger from, const NSInteger step) {
+    const NSInteger to = KSDatesCurrentYear();
+    
+    return KSDatesYears(from, to, step);
+}
+
+static inline NSMutableArray *KSDatesYearsFromCurrentYearToYear(const NSInteger to, const NSInteger step) {
+    const NSInteger from = KSDatesCurrentYear();
+    
+    return KSDatesYears(from, to, step);
+}
+
+NSDate *KSDatesDatePlusYears(NSDate *date, const NSInteger years);
+
+#pragma mark - Month
+
+NSInteger KSDatesMonth(NSDate *date);
+
+static inline NSInteger KSDatesCurrentMonth() {
+    return KSDatesMonth([NSDate date]);
+}
+
+static inline NSArray *KSDatesMonthStrings() {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    NSArray *months = [formatter monthSymbols];
+    [formatter release];
+    return months;
+}
+
+static inline NSString *KSDatesMonthString(const NSUInteger month) {
+    return [KSDatesMonthStrings() objectAtIndex:month];
+}
+
+#pragma mark - Day
+
+NSDate *KSDatesDateFromComponents(NSUInteger year, NSUInteger month, NSUInteger day);
+
+static inline NSInteger KSDatesDay(NSDate *date) {
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSDayCalendarUnit
+                                                                   fromDate:date];
+    
+    return components.day;
+}
+
+#pragma mark - ISO
+
+NSString *KSDatesISOString(NSDate *date);
+
+NSDate *KSDatesISODate(NSString *string);
+
+NSDate *KSDatesISODateTime(NSString *string);
+
+#pragma mark - Date
+
+BOOL KSDatesIsPastDay(NSDate *date);
+
+NSInteger KSDatesDistanceInDays(NSDate *from, NSDate *to);
+
+NSDate *KSDatesDatePlusDays(NSDate *date, NSInteger days);
---
+++ Components/Classes/KSMacros/KSMacrosDates.m
@@ -0,0 +1,163 @@
+//
+// Copyright (c) 2014, Davide De Rosa
+// All rights reserved.
+//
+// This code is distributed under the terms and conditions of the BSD license.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "KSMacrosDates.h"
+
+typedef struct {
+    NSInteger day;
+    NSInteger month;
+} KSDatesMonthDay;
+
+#pragma mark - Year
+
+NSInteger KSDatesYear(NSDate *date) {
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSYearCalendarUnit
+                                                                   fromDate:date];
+    
+    return components.year;
+}
+
+NSMutableArray *KSDatesYears(const NSInteger from, const NSInteger to, const NSInteger step) {
+    NSInteger year = from;
+    NSInteger lastYear = to;
+    
+    if (step < 0) {
+        year = to;
+        lastYear = from;
+    }
+    
+    // include last
+    lastYear += step;
+    
+    NSMutableArray *years = [NSMutableArray array];
+    while (year != lastYear) {
+        [years addObject:[NSNumber numberWithInteger:year]];
+        year += step;
+    }
+    return years;
+}
+
+NSDate *KSDatesDatePlusYears(NSDate *date, const NSInteger years) {
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    components.year = years;
+    NSDate *offsetDate = [[NSCalendar currentCalendar] dateByAddingComponents:components toDate:date options:0];
+    [components release];
+    
+    return offsetDate;
+}
+
+#pragma mark - Month
+
+NSInteger KSDatesMonth(NSDate *date) {
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSMonthCalendarUnit
+                                                                   fromDate:date];
+    
+    return components.month;
+}
+
+#pragma mark - ISO
+
+NSString *KSDatesISOString(NSDate *date) {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd";
+    NSString *string = [formatter stringFromDate:date];
+    [formatter release];
+    return string;
+}
+
+NSDate *KSDatesISODate(NSString *string) {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd";
+    NSDate *date = [formatter dateFromString:string];
+    [formatter release];
+    return date;
+}
+
+NSDate *KSDatesISODateTime(NSString *string) {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+    NSDate *date = [formatter dateFromString:string];
+    [formatter release];
+    return date;
+}
+
+#pragma mark - Date
+
+NSDate *KSDatesDateFromComponents(NSUInteger year, NSUInteger month, NSUInteger day)
+{
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    components.year = year;
+    components.month = month;
+    components.day = day;
+    return [[[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar] dateFromComponents:components];
+}
+
+BOOL KSDatesIsPastDay(NSDate *date) {
+    //    return ([date compare:[NSDate date]] == NSOrderedAscending);
+    
+    if (!date) {
+        return NO;
+    }
+    
+    // giorni dalla data parametro ad oggi
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSDayCalendarUnit
+                                                                   fromDate:[NSDate date]
+                                                                     toDate:date
+                                                                    options:0];
+    
+    // oggi o futura (0 o più giorni da oggi)
+    const BOOL presenteOFutura = (components.day >= 0);
+    
+    // più comprensibile
+    return !presenteOFutura;
+}
+
+NSInteger KSDatesDistanceInDays(NSDate *from, NSDate *to) {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    const NSUInteger units = NSDayCalendarUnit | NSMonthCalendarUnit | NSYearCalendarUnit;
+    NSDateComponents *fromComponents = [calendar components:units fromDate:from];
+    NSDateComponents *toComponents = [calendar components:units fromDate:to];
+    
+    NSDateComponents *components = [calendar components:NSDayCalendarUnit
+                                               fromDate:[calendar dateFromComponents:fromComponents]
+                                                 toDate:[calendar dateFromComponents:toComponents]
+                                                options:0];
+    
+    return components.day;
+}
+
+NSDate *KSDatesDatePlusDays(NSDate *date, NSInteger days) {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *components = [[NSDateComponents alloc] init];
+    components.day = days;
+    
+    NSDate *plus = [calendar dateByAddingComponents:components toDate:date options:0];
+    [components release];
+    
+    return plus;
+}
