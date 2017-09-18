keeshux/ios-components

commit 6a621283160cd5b6b5a2f381f01a6aef4993c8c7

Added new color macros
Breaks KSUIColorFromRGB(), function name wasn't appropriate.

Components/Macros/KSMacrosUI.h
@@ -97,13 +97,26 @@ static inline BOOL KSUIIsPhone5OrLater()
     return (KSUIIsPhone() && (KSUIScreenMaxLength() >= 568.0));
 }
 
-static inline UIColor *KSUIColorFromRGB(const NSUInteger rgb)
+static inline UIColor *KSUIColorFromRGBA(const NSUInteger r, const NSUInteger g, const NSUInteger b, const NSUInteger a)
 {
-    const CGFloat r = ((rgb >> 16) & 0xFF) / 255.0;
-    const CGFloat g = ((rgb >> 8) & 0xFF) / 255.0;
-    const CGFloat b = (rgb & 0xFF) / 255.0;
+    return [UIColor colorWithRed:(r / 255.0)
+                           green:(g / 255.0)
+                            blue:(b / 255.0)
+                           alpha:(a / 255.0)];
+}
+
+static inline UIColor *KSUIColorFromRGB(const NSUInteger r, const NSUInteger g, const NSUInteger b)
+{
+    return KSUIColorFromRGBA(r, g, b, 255.0);
+}
+
+static inline UIColor *KSUIColorFromHex(const NSUInteger hex)
+{
+    const CGFloat r = ((hex >> 16) & 0xFF) / 255.0;
+    const CGFloat g = ((hex >> 8) & 0xFF) / 255.0;
+    const CGFloat b = (hex & 0xFF) / 255.0;
     
-    return [UIColor colorWithRed:r green:g blue:b alpha:1.0];
+    return KSUIColorFromRGB(r, g, b);
 }
 
 #define KSUISF(format, ...)     [NSString stringWithFormat:format, __VA_ARGS__]

commit 7bf20668352f18023753e6c011e803036ed44e87

Fix KSUIColorFromHex macro
Broken by 6a62128.

Components/Macros/KSMacrosUI.h
@@ -112,9 +112,9 @@ static inline UIColor *KSUIColorFromRGB(const NSUInteger r, const NSUInteger g, 
 
 static inline UIColor *KSUIColorFromHex(const NSUInteger hex)
 {
-    const CGFloat r = ((hex >> 16) & 0xFF) / 255.0;
-    const CGFloat g = ((hex >> 8) & 0xFF) / 255.0;
-    const CGFloat b = (hex & 0xFF) / 255.0;
+    const CGFloat r = (hex >> 16) & 0xFF;
+    const CGFloat g = (hex >> 8) & 0xFF;
+    const CGFloat b = hex & 0xFF;
     
     return KSUIColorFromRGB(r, g, b);
 }